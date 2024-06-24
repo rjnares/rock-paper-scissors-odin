8b10526998529e0c6d175edc227d5e5dad5c4c8d
Roberto Nares <roberto.nares@gmail.com>
2024-06-24 13:03:11 -0700

    Implement playGame function
    Moved human and cpu score variables to playGame function. Moved playRound function inside of playGame function. Simulated 5 rounds being played and a winner declared at the end.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     // Get random integer in range [0, 2]
     const randomInteger = Math.floor(Math.random() * 3);
@@ -28,29 +25,48 @@ function getHumanChoice() {
     return input;
 }
 
-function playRound(humanChoice, computerChoice) {
-    // All combos where human choice beats computer choice
-    if ((humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice === "scissors" && computerChoice === "paper")) {
-        
-        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
-        computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore++;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        // All combos where human choice beats computer choice
+        if ((humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")) {
+            
+            humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+            computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        }
+        // All combos where human choice loses to computer choice
+        else if ((computerChoice === "rock" && humanChoice === "scissors") ||
+                (computerChoice === "paper" && humanChoice === "rock") ||
+                (computerChoice === "scissors" && humanChoice === "paper")) {
+            
+            humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+            computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        }
+        // All combos where human choice and computer choice are the same
+        else {
+            console.log("It's a tie!");
+        }
     }
-    // All combos where human choice loses to computer choice
-    else if ((computerChoice === "rock" && humanChoice === "scissors") ||
-             (computerChoice === "paper" && humanChoice === "rock") ||
-             (computerChoice === "scissors" && humanChoice === "paper")) {
-        
-        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
-        computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        computerScore++;
+
+    for (let round = 1; round <= 5; round++) {
+        playRound(getHumanChoice(), getComputerChoice());
     }
-    // All combos where human choice and computer choice are the same
+
+    if (humanScore > computerScore) {
+        console.log(`Game Over! You won ${humanScore} to ${computerScore}.`);
+    }
+    else if (computerScore > humanScore) {
+        console.log(`Game Over! You lost ${humanScore} to ${computerScore}.`);
+    }
     else {
-        console.log("It's a tie!");
+        console.log(`Game Over! You tied ${humanScore} to ${computerScore}.`);
     }
 }
