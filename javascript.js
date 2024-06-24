c35637a590c9e8ff5e57a6a57cd0b757ff3ee45a
Roberto Nares <roberto.nares@gmail.com>
2024-06-24 11:57:07 -0700

    Implement function to generate computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    // Returns random integer in range [0, 2]
+    const randomInt = Math.floor(Math.random() * 3);
+    let move;
+
+    if (randomInt === 0) {
+        move = "rock";
+    }
+    else if (randomInt === 1) {
+        move = "paper";
+    }
+    else {
+        move = "scissors";
+    }
+
+    return move;
+}
