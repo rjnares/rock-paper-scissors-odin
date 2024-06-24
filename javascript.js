5cd386d4ff1eeb28809389b8008fe8ac2bada2c7
Roberto Nares <roberto.nares@gmail.com>
2024-06-24 12:25:34 -0700

    Implement getHumanChoice function and refactor getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,17 +1,26 @@
 function getComputerChoice() {
-    // Returns random integer in range [0, 2]
-    const randomInt = Math.floor(Math.random() * 3);
-    let move;
+    // Get random integer in range [0, 2]
+    const randomInteger = Math.floor(Math.random() * 3);
 
-    if (randomInt === 0) {
-        move = "rock";
-    }
-    else if (randomInt === 1) {
-        move = "paper";
-    }
-    else {
-        move = "scissors";
-    }
+    if (randomInteger === 0) return "rock";
+    if (randomInteger === 1) return "paper";
 
-    return move;
+    return "scissors";
+}
+
+function getHumanChoice() {
+    let input;
+
+    do {
+        input = prompt("Enter rock, paper, or scissors");
+
+        // If player cancels prompt, then default human choice to rock
+        if (input === null) return "rock";
+        
+        // Trim input whitespace and make all lowercase for easier comparisons
+        input = input.trim().toLowerCase();
+    }
+    while (input !== "rock" && input !== "paper" && input !== "scissors");
+
+    return input;
 }
