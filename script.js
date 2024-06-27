13dda997eb18bc66a3e4e70536666c22314030ac
Roberto Nares <roberto.nares@gmail.com>
2024-06-27 00:19:53 -0700

    Remove unused code and use constants for move options

--- script.js
+++ script.js
@@ -1,77 +1,8 @@
-function getComputerChoice() {
-    // Get random integer in range [0, 2]
-    const randomInteger = Math.floor(Math.random() * 3);
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
 
-    if (randomInteger === 0) return "rock";
-    if (randomInteger === 1) return "paper";
-
-    return "scissors";
-}
-
-function getHumanChoice() {
-    let input;
-
-    do {
-        input = prompt("Enter rock, paper, or scissors");
-
-        // If player cancels prompt, then default human choice to rock
-        if (input === null) return "rock";
-        
-        // Trim input whitespace and make all lowercase for easier comparisons
-        input = input.trim().toLowerCase();
-    }
-    while (input !== "rock" && input !== "paper" && input !== "scissors");
-
-    return input;
-}
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-        // All combos where human choice beats computer choice
-        if ((humanChoice === "rock" && computerChoice === "scissors") ||
-            (humanChoice === "paper" && computerChoice === "rock") ||
-            (humanChoice === "scissors" && computerChoice === "paper")) {
-            
-            humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
-            computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        }
-        // All combos where human choice loses to computer choice
-        else if ((computerChoice === "rock" && humanChoice === "scissors") ||
-                (computerChoice === "paper" && humanChoice === "rock") ||
-                (computerChoice === "scissors" && humanChoice === "paper")) {
-            
-            humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
-            computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
-        // All combos where human choice and computer choice are the same
-        else {
-            console.log("It's a tie!");
-        }
-    }
-
-    for (let round = 1; round <= 5; round++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if (humanScore > computerScore) {
-        console.log(`Game Over! You won ${humanScore} to ${computerScore}.`);
-    }
-    else if (computerScore > humanScore) {
-        console.log(`Game Over! You lost ${humanScore} to ${computerScore}.`);
-    }
-    else {
-        console.log(`Game Over! You tied ${humanScore} to ${computerScore}.`);
-    }
-}
-
-const validOptions = ["rock", "paper", "scissors"];
+const validOptions = [ROCK, PAPER, SCISSORS];
 
 const message = document.querySelector("#message");
 const options = document.querySelector("#options");
@@ -86,6 +17,15 @@ options.addEventListener("click", (event) => {
         playRound(event.target.id, getComputerChoice());
     }
 });
+
+function getComputerChoice() {
+    const randomInteger = Math.floor(Math.random() * 3);
+
+    if (randomInteger === 0) return ROCK;
+    if (randomInteger === 1) return PAPER;
+
+    return SCISSORS;
+}
 
 function playRound(playerChoice, cpuChoice) {
     let newMessage = `You chose ${playerChoice} and the CPU chose ${cpuChoice}.`;
@@ -116,7 +56,7 @@ function playRound(playerChoice, cpuChoice) {
 }
 
 function playerWinsRound(playerChoice, cpuChoice) {
-    return playerChoice === "rock" && cpuChoice === "scissors" ||
-           playerChoice === "paper" && cpuChoice === "rock" ||
-           playerChoice === "scissors" && cpuChoice === "paper";
+    return playerChoice === ROCK && cpuChoice === SCISSORS ||
+           playerChoice === PAPER && cpuChoice === ROCK ||
+           playerChoice === SCISSORS && cpuChoice === PAPER;
 }
